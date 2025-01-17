5b0b1f6fb98ac5be87031273842a6131f6dbb47b
Nathan Hollis <natehollis@gmail.com>
2025-01-17 16:52:29 -0600

    add ts converter

---
+++ src/PolygonApi/Converters/UnixTimestampConverter.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace PolygonApi.Converters;
+
+public class UnixTimestampConverter : JsonConverter<DateTime>
+{
+    public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        // Convert Unix timestamp (in milliseconds) to DateTime
+        var timestamp = reader.GetInt64();
+        return DateTimeOffset.FromUnixTimeMilliseconds(timestamp).UtcDateTime;
+    }
+
+    public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+    {
+        // Convert DateTime back to Unix timestamp (in milliseconds)
+        var timestamp = new DateTimeOffset(value).ToUnixTimeMilliseconds();
+        writer.WriteNumberValue(timestamp);
+    }
+}
