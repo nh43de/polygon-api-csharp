412c4b6f036b6fd0691832efa920130770f18ed0
Nathan Hollis <natehollis@gmail.com>
2025-01-17 16:52:17 -0600

    service collection extensions

---
+++ src/PolygonApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace PolygonApi.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddPolygonApiClient(
+        this IServiceCollection services,
+        string baseUrl,
+        string apiKey)
+    {
+        services.AddRefitClient<IPolygonApi>()
+            .ConfigureHttpClient(client =>
+            {
+                client.BaseAddress = new Uri(baseUrl);
+                client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+            });
+
+        return services;
+    }
+}
